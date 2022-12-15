b824864b5ffa64e50c69fe58cc59230bdf5da5fb
Wale Osho <90667947+wbtech@users.noreply.github.com>
2022-12-15 23:33:56 +0100

    Update ImageShow.js

--- src/Component/ImageShow.js
+++ src/Component/ImageShow.js
@@ -9,28 +9,37 @@ import batman6 from "./Images/batman6.jpg";
 import batman7 from "./Images/batman7.jpg";
 
 const imageBox = {
-  height: "40vh",
+  height: "40%",
   display: "flex",
   flexDirection: "column",
+  backgroundColor: "ligthblue",
   justifyContent: "center",
   alignItems: "center",
   margin: "1rem",
 };
 
 const imageContainer = {
-  width: "40%",
+  width: "400px",
   overflow: "hidden",
+  margin: "1rem",
   boxShadow: "0 0 10px orange",
-  borderadius: "2rem",
+  // borderadius: "2rem",
 };
 
-const moving = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  transform: "translateX(0px)",
-  transition: "transform 0.3 linear",
-};
+const moves = document.getElementById("move");
+const images = document.querySelectorAll("img");
+console.log(images);
+let start = 0;
+console.log(moves);
+
+function move() {
+  start++;
+  if (start > images.length - 1) {
+    start = 0;
+  }
+  moves.style = `transform: translateX(${-start * 80}%)`;
+}
+setInterval(move, 5000);
 
 const ImageShow = () => {
   return (
@@ -38,7 +47,7 @@ const ImageShow = () => {
       <div style={imageBox}>
         <h2>Out Now</h2>
         <div style={imageContainer}>
-          <div style={moving} id='move'>
+          <div className='moving' id='move'>
             <img src={batman2} alt='batman' />
             <img src={batman3} alt='batman' />
             <img src={batman4} alt='batman' />
@@ -51,19 +60,5 @@ const ImageShow = () => {
     </>
   );
 };
-
-const moves = document.getElementById("move");
-const images = document.querySelectorAll("img");
-let start = 0;
-console.log(moves);
-
-function move() {
-  start++;
-  if (start > images.length - 1) {
-    start = 0;
-  }
-  moves.style.transform = `translateX(${-start * 80}%)`;
-}
-setInterval(move, 4000);
 
 export default ImageShow;
